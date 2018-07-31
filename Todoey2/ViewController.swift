63252fb064284f8cdc40790d99839a55fb24c881
RYOICHI MITSUMASU <ryoichimitsumasu@ryoichis-macbook-air.local>
2018-07-31 14:34:01 -0700

    Initial Commit

---
+++ Todoey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey2
+//
+//  Created by RYOICHI MITSUMASU on 7/31/18.
+//  Copyright © 2018 RYOICHI MITSUMASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
